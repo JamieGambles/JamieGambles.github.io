01c1b8a180ce0de35452b41300f8ba57977a5250
Jamie <Jamiegambles01@gmail.com>
2021-09-22 18:15:49 +1200

    thumbnails

---
+++ script.js
@@ -0,0 +1,8 @@
+var resizeTimer;
+window.addEventListener("resize", function () {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function () {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
